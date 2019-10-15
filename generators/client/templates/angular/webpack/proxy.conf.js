91cfa25a39d977a97f178edaba374a18716c62e5
William Marques <wmarques@ippon.fr>
2019-10-15 17:06:24 +0200

    Add config for dev and prod mode

--- generators/client/templates/angular/webpack/proxy.conf.js
+++
@@ -1,9 +0,0 @@
-const PROXY_CONFIG = [
-    {
-      context: ['/api', '/services', '/management', '/swagger-resources', '/v2/api-docs', '/h2-console', '/auth'],
-      target: `http://localhost:8080`,
-      secure: false
-    }
-  ];
-
-  module.exports = PROXY_CONFIG;
